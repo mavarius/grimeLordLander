a8ea9dce85da4140fcca8b6f9327be958c7c7dee
mavarius <metalwhirlwind@yahoo.com>
2017-01-13 12:36:26 -0800

    mailing action complete

--- js/index.js
+++ js/index.js
@@ -1,23 +1,39 @@
-$(document).ready(function() {
-  // EMAIL VALIDATION
+$(document).ready(function () {
+  // SEND MAIL
   $(function () {
+    var form = $('#ajax-contact')
+    var formMessages = $('#form-messages')
 
-        $('form').on('submit', function (e) {
+    $(form).submit(function(event) {
+      event.preventDefault()
 
-          e.preventDefault();
+      var formData = $(form).serialize()
 
-          $.ajax({
-            type: 'post',
-            url: 'post.php',
-            data: $('form').serialize(),
-            success: function () {
-              alert('form was submitted');
-            }
-          });
+      $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+      }).done(function(response) {
+        $(formMessages).removeClass('error');
+        $(formMessages).addClass('success');
 
-        });
+        $(formMessages).text(response);
 
-      });
-});
+        $('#name').val('')
+        $('#email').val('')
+      }).fail(function(data) {
+        $(formMessages).removeClass('success');
+        $(formMessages).addClass('error');
+
+        if (data.responseText !== '') {
+          $(formMessages).text(data.responseText);
+        } else {
+          $(formMessages).text('Oops! An error occured and your request could not be sent.');
+        }
+      })
+    })
+  })
+})
+
 // sass --watch scss:css
 // python -m SimpleHTTPServer
